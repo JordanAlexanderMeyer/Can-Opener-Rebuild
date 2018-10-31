c30f9fa0d257e09d7c24fb922a5721c28d3dba5d
Jordan Alexander Meyer <mac@wirelessprv-10-193-237-209.near.illinois.edu>
2018-10-30 20:37:01 -0500

    changed words on index

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -52,11 +52,11 @@ function roomChooser(topicValue, sideValue) {
 function populate() {
 	side.innerHTML = "";
 	if(topic.value == "default") {
-		var optionArray = ["default|Choose Your Side"];
+		var optionArray = ["default|Choose a Side"];
 	} else if(topic.value == "abortion") {
-		var optionArray = ["default|Choose Your Side", "pro_life|Pro-Life", "pro_choice|Pro-Choice"];
+		var optionArray = ["default|Choose a Side", "pro_life|Pro-Life", "pro_choice|Pro-Choice"];
 	} else if(topic.value == "gun_control") {
-		var optionArray = ["default|Choose Your Side", "for|For", "against|Against"];
+		var optionArray = ["default|Choose a Side", "for|For", "against|Against"];
 	}
 	for(var option in optionArray) {
 		var pair = optionArray[option].split("|");

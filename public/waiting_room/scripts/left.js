c98dee32748d5bf852367813b5efb35ff9c6dcf6
Jordan Alexander Meyer <mac@macs-MacBook.local>
2018-11-02 17:22:13 -0500

    initialized the variables from url earlier so they can be used by socket.io

--- public/waiting_room/scripts/left.js
+++ public/waiting_room/scripts/left.js
@@ -1,9 +1,3 @@
-var urlParams = new URLSearchParams(window.location.search);
-var topicValue = urlParams.get('topic');
-var sideValue = urlParams.get('side');
-var roomValue = urlParams.get('room');
-var status = '';
-
 // Remove the user from queue if they leave the waiting room before being paired. Doesn't work on mobile!
 //window.onbeforeunload = removeFromQueue;
 

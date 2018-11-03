3bded8d2af8d40c59f7ff6d2d5e902c587d49446
Jordan Alexander Meyer <mac@macs-MacBook.local>
2018-11-03 01:50:29 -0500

    changed out video chat app from appr.tc for tokbox

--- public/call_room/scripts/left.js
+++ public/call_room/scripts/left.js
@@ -3,33 +3,4 @@ var topicValue = urlParams.get('topic');
 var sideValue = urlParams.get('side');
 var roomValue = urlParams.get('room');
 
-document.getElementById('videochat').src = "https://appr.tc/r/" + roomValue;
-
-// If the user leaves the waiting room before being paired, remove them from the queue
-window.onbeforeunload = removeFromQueue;
-
-function removeFromQueue() {
-	// Remove the user from the queue it is in
-	var query = `query DelNum($topicValue: String!, $sideValue: String!, $roomValue: String!) {
-		delNum(topic: $topicValue, side: $sideValue, room: $roomValue)
-	}`;
-
-	fetch('/graphql', {
-	 	method: 'POST',
-	 	headers: {
-	  		'Content-Type': 'application/json',
-	    	'Accept': 'application/json',
-	  	},
-	  	body: JSON.stringify({
-	    	query,
-	    	variables: { topicValue, sideValue, roomValue },
-	  	})
-	})
-	.then(r => r.json())
-	.then(function(data) {
-		console.log(data['data']['delNum']);
-	})
-	.catch(function(error) {
-		console.log(error);
-	});
-}
+document.getElementById('videochat').src = `https://tokbox.com/embed/embed/ot-embed.js?embedId=08dcf5e1-909b-4fe1-876b-64527e849d41&room=${roomValue}&iframe=true`;
